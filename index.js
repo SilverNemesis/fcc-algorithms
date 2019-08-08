1486775a8b6cd02ad0a2e88a95a1511dea1dd985
Donald Fries <donald.darklight@gmail.com>
2019-08-08 17:49:09 -0500

    added more Rosetta code challenges

--- index.js
+++ index.js
@@ -296,6 +296,398 @@ function dfs(graph, root) {
   return visited
 }
 
+// Rosetta Code: 100 doors
+// There are 100 doors in a row that are all initially closed. You make 100 passes by the doors.
+// The first time through, visit every door and 'toggle' the door(if the door is closed, open it; if it is open, close it).
+// The second time, only visit every 2nd door(i.e., door #2, #4, #6, ...) and toggle it.
+// The third time, visit every 3rd door(i.e., door #3, #6, #9, ...), etc., until you only visit the 100th door.
+// Implement a function to determine the state of the doors after the last pass. Return the final result in an array, with only the door number included in the array if it is open.
+function getFinalOpenedDoors(numDoors) {
+  const doors = []
+
+  for (let door = 0; door < numDoors; door++) {
+    doors.push(false)
+  }
+
+  for (let pass = 0; pass < numDoors; pass++) {
+    for (let door = pass; door < numDoors; door += pass + 1) {
+      doors[door] = doors[door] == false
+    }
+  }
+
+  const results = []
+
+  for (let door = 0; door < numDoors; door++) {
+    if (doors[door]) {
+      results.push(door + 1)
+    }
+  }
+
+  return results
+}
+
+function solve24(numStr) {
+  const chars = numStr.split('')
+  const perms = []
+
+  function swap(i, j) {
+    const temp = chars[i]
+    chars[i] = chars[j]
+    chars[j] = temp
+  }
+
+  // Heap's algorithm (https://en.wikipedia.org/wiki/Heap%27s_algorithm)
+  function generate(len) {
+    if (len === 1) {
+      const perm = chars.join('')
+      if (!perms.includes(perm)) {
+        perms.push(perm)
+      }
+    } else {
+      for (let i = 0; i < len; i++) {
+        generate(len - 1)
+        if (len % 2 != 0) {
+          swap(0, len - 1)
+        } else {
+          swap(i, len - 1)
+        }
+      }
+    }
+  }
+
+  generate(chars.length)
+
+  const ops = ['+', '-', '*', '/']
+
+  let answer = undefined
+
+  const tryEval = (perms, routine) => {
+    perms.some((perm) => {
+      for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+          for (let k = 0; k < 4; k++) {
+            answer = routine(perm, ops, i, j, k)
+            if (answer) {
+              return true
+            }
+          }
+        }
+      }
+    })
+  }
+
+  const simpleRoutine = (perm, ops, i, j, k) => {
+    const exp = perm[0] + ops[i] + perm[1] + ops[j] + perm[2] + ops[k] + perm[3]
+    if (eval(exp) == 24) {
+      return exp
+    }
+  }
+
+  const parenRoutine = (perm, ops, i, j, k) => {
+    const exp = '(' + perm[0] + ops[i] + perm[1] + ')' + ops[j] + '(' + perm[2] + ops[k] + perm[3] + ')'
+    if (eval(exp) == 24) {
+      return exp
+    }
+  }
+
+  /*
+  const parenRoutine2 = (perm, ops, i, j, k) => {
+    const exp = perm[0] + ops[i] + '(' + perm[1] + ops[j] + '(' + perm[2] + ops[k] + perm[3] + '))'
+    if (eval(exp) == 24) {
+      return exp
+    }
+  }
+
+  const parenRoutine3 = (perm, ops, i, j, k) => {
+    const exp = '((' + perm[0] + ops[i] + perm[1] + ')' + ops[j] + perm[2] + ')' + ops[k] + perm[3]
+    if (eval(exp) == 24) {
+      return exp
+    }
+  }
+
+  const parenRoutine4 = (perm, ops, i, j, k) => {
+    const exp = '(' + perm[0] + ops[i] + '(' + perm[1] + ops[j] + perm[2] + '))' + ops[k] + perm[3]
+    if (eval(exp) == 24) {
+      return exp
+    }
+  }
+  */
+
+  tryEval(perms, simpleRoutine)
+  if (answer) {
+    return answer
+  }
+
+  tryEval(perms, parenRoutine)
+  if (answer) {
+    return answer
+  }
+}
+
+function numberOfNames(num) {
+  const table = []
+  const partition = (sum, max) => {
+    if (max == 0) {
+      return 0
+    } else if (sum == 0) {
+      return 1
+    } else if (sum < 0) {
+      return 0
+    }
+
+    const index = sum * 200 + max
+
+    if (sum < 200 && max < 200) {
+      if (table[index])
+        return table[index]
+      else {
+        table[index] = partition(sum, max - 1) + partition(sum - max, max)
+        return table[index]
+      }
+    } else {
+      return partition(sum, max - 1) + partition(sum - max, max)
+    }
+  }
+
+  return partition(num, num - 1) + 1
+}
+
+function canMakeWord(word) {
+  const blocks = [
+    ['B', 'O'],
+    ['X', 'K'],
+    ['D', 'Q'],
+    ['C', 'P'],
+    ['N', 'A'],
+    ['G', 'T'],
+    ['R', 'E'],
+    ['T', 'G'],
+    ['Q', 'D'],
+    ['F', 'S'],
+    ['J', 'W'],
+    ['H', 'U'],
+    ['V', 'I'],
+    ['A', 'N'],
+    ['O', 'B'],
+    ['E', 'R'],
+    ['F', 'S'],
+    ['L', 'Y'],
+    ['P', 'C'],
+    ['Z', 'M']
+  ]
+  const used = []
+  const letters = [...word.toUpperCase()]
+  const result = letters.some((letter) => {
+    const index = blocks.findIndex((block, i) => {
+      return (block[0] == letter || block[1] == letter) && !used.includes(i)
+    })
+    if (index == -1) {
+      return true
+    }
+    used.push(index)
+  })
+  return !result
+}
+
+function getDPA(num) {
+  let a = 0, d = 0, p = 0
+  const sums = new Array(num + 1).fill(0)
+  for (let divisor = 1; divisor <= num / 2; divisor++) {
+    for (let i = divisor + divisor; i <= num; i += divisor) {
+      sums[i] += divisor
+    }
+  }
+  for (let i = 1; i <= num; i++) {
+    if (sums[i] < i) {
+      d++
+    } else if (sums[i] > i) {
+      a++
+    } else {
+      p++
+    }
+  }
+  return [d, p, a]
+}
+
+function accumulator(sum) {
+  return function (value) {
+    sum += value
+    return sum
+  }
+}
+
+function ack(m, n) {
+  if (m === 0) {
+    return n + 1
+  } else if (n == 0) {
+    return ack(m - 1, 1)
+  } else if (m > 0 && n > 0) {
+    return ack(m - 1, ack(m, n - 1))
+  }
+}
+
+function formatText(input, justification) {
+  const columnWidths = []
+  input.forEach((row) => {
+    const cols = row.split('$')
+    cols.forEach((col, i) => {
+      const len = col.length
+      if (!columnWidths[i] || len > columnWidths[i]) {
+        columnWidths[i] = len
+      }
+    })
+  })
+
+  const lines = []
+  input.forEach((row, j) => {
+    const cols = row.split('$')
+    line = []
+    cols.forEach((col, i) => {
+      const len = col.length
+      let cell
+      if (len > 0) {
+        const wid = columnWidths[i]
+        switch (justification) {
+          case 'right':
+            cell = col.padStart(wid, ' ')
+            break
+          case 'left':
+            cell = col.padEnd(wid, ' ')
+            break
+          case 'center':
+            const beg = (wid - len) / 2
+            cell = col.padStart(beg + len, ' ').padEnd(wid, ' ')
+            break
+        }
+      }
+      line.push(cell)
+    })
+    lines.push(line.join(' '))
+  })
+  return lines.join('\n')
+}
+
+function amicablePairsUpTo(maxNum) {
+  function getSums(num) {
+    const sums = new Array(num + 1).fill(0)
+    for (let divisor = 1; divisor <= num / 2; divisor++) {
+      for (let i = divisor + divisor; i <= num; i += divisor) {
+        sums[i] += divisor
+      }
+    }
+    return sums
+  }
+
+  const sums = getSums(maxNum)
+  const pairs = []
+  for (let i = 1; i <= maxNum; i++) {
+    const s = sums[i]
+    if (s !== i && sums[s] === i) {
+      if (!pairs.some((v) => v[0] === s && v[1] === i)) {
+        pairs.push([i, s])
+      }
+    }
+  }
+  return pairs
+}
+
+function mode(arr) {
+  let frequency = new Map()
+  let maxFreq = 0
+  arr.forEach((value) => {
+    let freq = frequency.get(value)
+    if (freq) {
+      freq++
+    } else {
+      freq = 1
+    }
+    if (freq > maxFreq) {
+      maxFreq = freq
+    }
+    frequency.set(value, freq)
+  })
+  const results = []
+  frequency.forEach((value, key) => {
+    if (value === maxFreq) {
+      results.push(key)
+    }
+  })
+  return results
+}
+
+function pythagoreanMeans(rangeArr) {
+  const n = rangeArr.length
+  const A = rangeArr.reduce((acc, cur) => acc + cur) / n
+  const G = Math.pow(rangeArr.reduce((acc, cur) => acc * cur), 1 / n)
+  const H = n / rangeArr.reduce((acc, cur) => acc + 1 / cur, 0)
+  return {
+    values: {
+      Arithmetic: A,
+      Geometric: G,
+      Harmonic: H
+    },
+    test: 'is A >= G >= H ? ' + (A >= G && G >= H ? 'yes' : 'no')
+  }
+}
+
+function rms(arr) {
+  const n = arr.length
+  return Math.sqrt(arr.reduce((acc, cur) => acc + cur * cur, 0) / n)
+}
+
+function babbage(babbageNum, endDigits) {
+  let answer = 0
+  for (let i = babbageNum; i > 0; i--) {
+    if ((i * i) % 1000000 === endDigits) {
+      answer = i
+    }
+  }
+  return answer
+}
+
+function isBalanced(str) {
+  let level = 0
+  const len = str.length
+  for (let i = 0; i < len; i++) {
+    if (str[i] == '[') {
+      level++
+    } else if (str[i] == ']') {
+      level--
+      if (level < 0) {
+        return false;
+      }
+    } else {
+      return false
+    }
+  }
+  return level === 0
+}
+
+function getCircles(...args) {
+  const p1 = args[0]
+  const p2 = args[1]
+  const r = args[2]
+  const d = Math.sqrt((p1[0] - p2[0]) * (p1[0] - p2[0]) + (p1[1] - p2[1]) * (p1[1] - p2[1]))
+  if (d === 0) {
+    if (r === 0) {
+      return 'Radius Zero'
+    } else {
+      return 'Coincident point.Infinite solutions'
+    }
+  } else if (d > r * 2) {
+    return 'No intersection.Points further apart than circle diameter'
+  } else if (d === r * 2) {
+    return [(p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2]
+  }
+
+  p3 = [(p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2]
+
+  return [
+    [Number((p3[0] + Math.sqrt(r * r - (d / 2) * (d / 2)) * (p1[1] - p2[1]) / d).toFixed(4)), Number((p3[1] + Math.sqrt(r * r - (d / 2) * (d / 2)) * (p2[0] - p1[0]) / d).toFixed(4))],
+    [Number((p3[0] - Math.sqrt(r * r - (d / 2) * (d / 2)) * (p1[1] - p2[1]) / d).toFixed(4)), Number((p3[1] - Math.sqrt(r * r - (d / 2) * (d / 2)) * (p2[0] - p1[0]) / d).toFixed(4))]
+  ]
+}
+
 module.exports = {
   findSymmetricDifference,
   updateInventory,
@@ -307,5 +699,20 @@ module.exports = {
   quickSort,
   mergeSort,
   bfs,
-  dfs
+  dfs,
+  getFinalOpenedDoors,
+  solve24,
+  numberOfNames,
+  canMakeWord,
+  getDPA,
+  accumulator,
+  ack,
+  formatText,
+  amicablePairsUpTo,
+  mode,
+  pythagoreanMeans,
+  rms,
+  babbage,
+  isBalanced,
+  getCircles
 }
--- index.test.js
+++ index.test.js
@@ -94,3 +94,253 @@ test('Implement Depth First Search', () => {
   expect(algorithms.dfs([[0, 1, 0, 0], [1, 0, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0]], 0))
     .toStrictEqual([0, 1])
 })
+
+test('Rosetta Code: 100 doors', () => {
+  expect(algorithms.getFinalOpenedDoors(100))
+    .toStrictEqual([1, 4, 9, 16, 25, 36, 49, 64, 81, 100])
+})
+
+test('Rosetta Code: 24 game', () => {
+  const check = (input) => {
+    return (answer) => {
+      return eval(answer) === 24
+    }
+  }
+  expect(algorithms.solve24('4878'))
+    .toSatisfy(check('4878'))
+  expect(algorithms.solve24('1234'))
+    .toSatisfy(check('1234'))
+  expect(algorithms.solve24('6789'))
+    .toSatisfy(check('6789'))
+  expect(algorithms.solve24('1127'))
+    .toSatisfy(check('1127'))
+})
+
+test('Rosetta Code: 9 billion names of God the integer', () => {
+  expect(algorithms.numberOfNames(5))
+    .toStrictEqual(7)
+  expect(algorithms.numberOfNames(12))
+    .toStrictEqual(77)
+  expect(algorithms.numberOfNames(18))
+    .toStrictEqual(385)
+  expect(algorithms.numberOfNames(23))
+    .toStrictEqual(1255)
+  expect(algorithms.numberOfNames(42))
+    .toStrictEqual(53174)
+  expect(algorithms.numberOfNames(123))
+    .toStrictEqual(2552338241)
+  expect(algorithms.numberOfNames(250))
+    .toStrictEqual(230793554364681)
+})
+
+test('Rosetta Code: ABC Problem', () => {
+  expect(algorithms.canMakeWord('bark'))
+    .toStrictEqual(true)
+  expect(algorithms.canMakeWord('BooK'))
+    .toStrictEqual(false)
+  expect(algorithms.canMakeWord('TReAT'))
+    .toStrictEqual(true)
+  expect(algorithms.canMakeWord('COMMON'))
+    .toStrictEqual(false)
+  expect(algorithms.canMakeWord('squAD'))
+    .toStrictEqual(true)
+  expect(algorithms.canMakeWord('conFUSE'))
+    .toStrictEqual(true)
+})
+
+test('Rosetta Code: Abundant, deficient and perfect number classifications', () => {
+  expect(algorithms.getDPA(6))
+    .toStrictEqual([5, 1, 0])
+  expect(algorithms.getDPA(10))
+    .toStrictEqual([9, 1, 0])
+  expect(algorithms.getDPA(100))
+    .toStrictEqual([76, 2, 22])
+  expect(algorithms.getDPA(1000))
+    .toStrictEqual([751, 3, 246])
+  expect(algorithms.getDPA(13195))
+    .toStrictEqual([9914, 4, 3277])
+  expect(algorithms.getDPA(20000))
+    .toStrictEqual([15043, 4, 4953])
+})
+
+test('Rosetta Code: Accumulator factory', () => {
+  const testAccumulator1 = algorithms.accumulator(0)
+  expect(testAccumulator1)
+    .toBeFunction()
+  expect(testAccumulator1(1))
+    .toStrictEqual(1)
+  expect(testAccumulator1(2))
+    .toStrictEqual(3)
+  expect(testAccumulator1(3))
+    .toStrictEqual(6)
+  expect(testAccumulator1(4))
+    .toStrictEqual(10)
+  const testAccumulator2 = algorithms.accumulator(3)
+  expect(testAccumulator2)
+    .toBeFunction()
+  expect(testAccumulator2(-4))
+    .toStrictEqual(-1)
+  expect(testAccumulator2(1.5))
+    .toStrictEqual(.5)
+  expect(testAccumulator2(5))
+    .toStrictEqual(5.5)
+})
+
+test('Rosetta Code: Ackermann function', () => {
+  expect(algorithms.ack(0, 0))
+    .toStrictEqual(1)
+  expect(algorithms.ack(1, 1))
+    .toStrictEqual(3)
+  expect(algorithms.ack(2, 5))
+    .toStrictEqual(13)
+  expect(algorithms.ack(3, 3))
+    .toStrictEqual(61)
+})
+
+test('Rosetta Code: Align columns', () => {
+  const input = [
+    'Given$a$text$file$of$many$lines',
+    'where$fields$within$a$line$',
+    'are$delineated$by$a$single$"dollar"$character',
+    'write$a$program',
+    'that$aligns$each$column$of$fields$',
+    'by$ensuring$that$words$in$each$',
+    'column$are$separated$by$at$least$one$space.',
+    'Further,$allow$for$each$word$in$a$column$to$be$either$left$',
+    'justified,$right$justified',
+    'or$center$justified$within$its$column.'
+  ];
+
+  const rightAligned =
+    '     Given          a      text   file     of     many     lines\n' +
+    '     where     fields    within      a   line \n' +
+    '       are delineated        by      a single "dollar" character\n' +
+    '     write          a   program\n' +
+    '      that     aligns      each column     of   fields \n' +
+    '        by   ensuring      that  words     in     each \n' +
+    '    column        are separated     by     at    least       one space.\n' +
+    '  Further,      allow       for   each   word       in         a column to be either left \n' +
+    'justified,      right justified\n' +
+    '        or     center justified within    its  column.'
+  expect(algorithms.formatText(input, 'right'))
+    .toStrictEqual(rightAligned)
+
+  const leftAligned =
+    'Given      a          text      file   of     many     lines    \n' +
+    'where      fields     within    a      line   \n' +
+    'are        delineated by        a      single "dollar" character\n' +
+    'write      a          program  \n' +
+    'that       aligns     each      column of     fields   \n' +
+    'by         ensuring   that      words  in     each     \n' +
+    'column     are        separated by     at     least    one       space.\n' +
+    'Further,   allow      for       each   word   in       a         column to be either left \n' +
+    'justified, right      justified\n' +
+    'or         center     justified within its    column. '
+  expect(algorithms.formatText(input, 'left'))
+    .toStrictEqual(leftAligned)
+
+  const centerAligned =
+    '  Given        a        text     file    of     many     lines  \n' +
+    '  where      fields    within     a     line  \n' +
+    '   are     delineated    by       a    single \"dollar\" character\n' +
+    '  write        a       program \n' +
+    '   that      aligns     each    column   of    fields  \n' +
+    '    by      ensuring    that    words    in     each   \n' +
+    '  column      are     separated   by     at    least      one    space.\n' +
+    ' Further,    allow       for     each   word     in        a     column to be either left \n' +
+    'justified,   right    justified\n' +
+    '    or       center   justified within  its   column. '
+  expect(algorithms.formatText(input, 'center'))
+    .toStrictEqual(centerAligned)
+})
+
+test('Rosetta Code: Amicable pairs', () => {
+  expect(algorithms.amicablePairsUpTo(300))
+    .toStrictEqual([[220, 284]])
+  expect(algorithms.amicablePairsUpTo(3000))
+    .toStrictEqual([[220, 284], [1184, 1210], [2620, 2924]])
+  expect(algorithms.amicablePairsUpTo(20000))
+    .toStrictEqual([[220, 284], [1184, 1210], [2620, 2924], [5020, 5564], [6232, 6368], [10744, 10856], [12285, 14595], [17296, 18416]])
+})
+
+test('Rosetta Code: Averages/Mode', () => {
+  expect(algorithms.mode([1, 3, 6, 6, 6, 6, 7, 7, 12, 12, 17]))
+    .toStrictEqual([6])
+  expect(algorithms.mode([1, 2, 4, 4, 1]))
+    .toStrictEqual([1, 4])
+})
+
+test('Rosetta Code: Averages/Pythagorean means', () => {
+  expect(algorithms.pythagoreanMeans([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+    .toStrictEqual({
+      values: {
+        Arithmetic: 5.5,
+        Geometric: 4.528728688116765,
+        Harmonic: 3.414171521474055
+      },
+      test: 'is A >= G >= H ? yes'
+    })
+})
+
+test('Rosetta Code: Averages/Root mean square', () => {
+  expect(algorithms.rms([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+    .toStrictEqual(6.2048368229954285)
+})
+
+test('Rosetta Code: Babbage problem', () => {
+  const check = (babbageNum, endDigits) => {
+    return (answer) => {
+      return answer < babbageNum && (answer * answer) % 1000000 === endDigits
+    }
+  }
+
+  expect(algorithms.babbage(99736, 269696))
+    .toSatisfy(check(99736, 269696))
+})
+
+test('Rosetta Code: Balanced brackets', () => {
+  expect(algorithms.isBalanced("[]"))
+    .toStrictEqual(true)
+  expect(algorithms.isBalanced("]][[[][][][]]["))
+    .toStrictEqual(false)
+  expect(algorithms.isBalanced("[][[[[][][[[]]]]]]"))
+    .toStrictEqual(true)
+  expect(algorithms.isBalanced("]["))
+    .toStrictEqual(false)
+  expect(algorithms.isBalanced("[[[]]]][[]"))
+    .toStrictEqual(false)
+  expect(algorithms.isBalanced("][[]"))
+    .toStrictEqual(false)
+  expect(algorithms.isBalanced("][[][]][[[]]"))
+    .toStrictEqual(false)
+  expect(algorithms.isBalanced("[[][]]]["))
+    .toStrictEqual(false)
+  expect(algorithms.isBalanced("[[[]]][[]]]][][["))
+    .toStrictEqual(false)
+  expect(algorithms.isBalanced("[]][[]]][[[[][]]"))
+    .toStrictEqual(false)
+  expect(algorithms.isBalanced("][]][[]["))
+    .toStrictEqual(false)
+  expect(algorithms.isBalanced("[[]][[][]]"))
+    .toStrictEqual(true)
+  expect(algorithms.isBalanced("[[]]"))
+    .toStrictEqual(true)
+  expect(algorithms.isBalanced("]][]][[]][[["))
+    .toStrictEqual(false)
+  expect(algorithms.isBalanced("][]][][["))
+    .toStrictEqual(false)
+  expect(algorithms.isBalanced("][]["))
+    .toStrictEqual(false)
+  expect(algorithms.isBalanced("[[]]][][][[]]["))
+    .toStrictEqual(false)
+  expect(algorithms.isBalanced(""))
+    .toStrictEqual(true)
+})
+
+test('Rosetta Code: Circles of given radius through two points', () => {
+  expect(algorithms.getCircles([0.1234, 0.9876], [0.8765, 0.2345], 2.0)).toStrictEqual([[1.8631, 1.9742], [-0.8632, -0.7521]])
+  expect(algorithms.getCircles([0.0000, 2.0000], [0.0000, 0.0000], 1.0)).toStrictEqual([0, 1])
+  expect(algorithms.getCircles([0.1234, 0.9876], [0.1234, 0.9876], 2.0)).toStrictEqual('Coincident point.Infinite solutions')
+  expect(algorithms.getCircles([0.1234, 0.9876], [0.8765, 0.2345], 0.5)).toStrictEqual('No intersection.Points further apart than circle diameter')
+  expect(algorithms.getCircles([0.1234, 0.9876], [0.1234, 0.9876], 0.0)).toStrictEqual('Radius Zero')
+})
